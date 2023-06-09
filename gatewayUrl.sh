d0ef1cb26cb0009c172066590badd693082ba2e5
AINULX00159358 <X00159358@myTUDublin.ie>
2023-06-09 01:01:54 +0100

    added gateway setting shell script

--- gatewayUrl.sh
+++ gatewayUrl.sh
@@ -1,5 +1,6 @@
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-
+echo "INGRESS_HOST set to ${INGRESS_HOST}"
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
-
+echo "INGRESS_PORT set to ${INGRESS_PORT}"
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+echo "GATEWAY_URL set to ${GATEWAY_URL}"
