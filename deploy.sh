11fb9b630faa9b45bae5ad0aa44815925dd8cc03
AINULX00159358 <X00159358@myTUDublin.ie>
2023-06-19 09:09:50 +0100

    prepare for canary deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-helm upgrade --install invoice-mgr ./helm
+
+
+while getopts ns:app:vs: flag
+do
+    case "${flag}" in
+        ns) deployns ${OPTARG};;
+        app) deployapp ${OPTARG};;
+        vs) deployvs ${OPTARG};;
+    esac
+done
+
+deployns() {
+  kubectl create namespaces $1
+  kubectl label namespace $1 name=$1 istio-injection=enabled
+}
+
+deployapp() {
+  helm upgrade --namespaces $1 --install invoice-mgr ./helm
+}
