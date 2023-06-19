14b743527617e0702dd6171f89a97942f4ca2b0b
Ainul.Habib <habib.ainul@fmr.com>
2023-06-19 15:33:27 +0100

    added gateway

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-
-while getopts ns:app:vs: flag
-do
-    case "${flag}" in
-        ns) deployns ${OPTARG};;
-        app) deployapp ${OPTARG};;
-        vs) deployvs ${OPTARG};;
-    esac
-done
+echo "starting $*"
 
 deployns() {
-  kubectl create namespaces $1
+  echo "create namespace $1"
+  kubectl create namespace $1
   kubectl label namespace $1 name=$1 istio-injection=enabled
+  kubectl get namespace $1 -o=yaml
 }
 
 deployapp() {
-  helm upgrade --namespaces $1 --install invoice-mgr ./helm
+  echo "create deploy app $1"
+  helm upgrade -n $1 --install invoice-manager-app ./helm
 }
+
+deployvs() {
+  echo "create deploy gateway $1"
+  helm upgrade -n $1 --install invoice-mgr ./gateway/helm
+}
+
+
+case "$1" in
+  "namespace")
+    deployns $2
+    ;;
+  "application")
+    deployapp $2
+    ;;
+  "virtualservice")
+     deployvs $2
+    ;;
+  *)
+    echo "You have failed to specify what to do correctly."
+    exit 1
+    ;;
+esac
