5e0387e8319097276c3f79ede3f037bf53b3fca0
AINULX00159358 <X00159358@myTUDublin.ie>
2023-06-20 18:25:11 +0100

    supportFile set to false

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'a7bq2k',
   e2e: {
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
