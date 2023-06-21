978138e76e94bdfd30f683a79214e1fc3dd97e4e
AINULX00159358 <X00159358@myTUDublin.ie>
2023-06-22 00:04:41 +0100

    set docer image

--- BuildDockerImage.sh
+++ BuildDockerImage.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-docker build . -t invoice-mgr-app:v1.0.0 -f docker/Dockerfile_app
-docker build . -t invoice-mgr-data:v1.0.0 -f docker/Dockerfile_data
-docker build . -t invoice-mgr-gen:v1.0.0 -f docker/Dockerfile_gen
-docker build . -t invoice-mgr-payment:v1.0.0 -f docker/Dockerfile_payment
+docker build . -t invoice-mgr:v1.0.0 -f docker/Dockerfile_gen
 docker images | grep invoice
