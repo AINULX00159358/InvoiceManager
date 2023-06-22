1f5cad4531394757c1e0c07f5b3e4ac322d32124
AINULX00159358 <X00159358@myTUDublin.ie>
2023-06-22 20:22:14 +0100

    updated code for azure

--- BuildDockerImage.sh
+++ BuildDockerImage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build . -t alixtudublincr.azurecr.io/invoice-mgr:v100 -f docker/Dockerfile
+docker build . -t alixtudublincr.azurecr.io/invoice-mgr:$1 -f docker/Dockerfile
 docker images | grep invoice
