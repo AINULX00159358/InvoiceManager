db0799eaed95f29df74f36dba9b7c344ed89ea51
Ainul.Habib <habib.ainul@fmr.com>
2023-06-25 11:29:20 +0100

    updated code

--- deploy-ns.sh
+++ deploy-ns.sh
@@ -2,4 +2,4 @@
 echo "create namespace $1"
 kubectl create namespace $1
 kubectl label namespace $1 name=$1 istio-injection=enabled
-kubectl get namespace $1 -o=yaml
+kubectl apply --namespace=$1 -f ./sa/invoice-mgr-sa.yaml
