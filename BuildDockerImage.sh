ac75e2bbd238f6b813f6fc0180165f63d636b61e
AINULX00159358 <X00159358@myTUDublin.ie>
2023-06-26 17:53:29 +0100

    builddocker

--- BuildDockerImage.sh
+++ BuildDockerImage.sh
@@ -2,4 +2,4 @@
 docker build . -t invoice-mgr:v1.1.0 -f docker/Dockerfile
 docker tag invoice-mgr:v1.1.0 x00159358/invoice-mgr:v1.1.0
 docker images | grep invoice
-docker push
+docker push x00159358/invoice-mgr:v1.1.0
