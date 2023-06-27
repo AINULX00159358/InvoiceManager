e16090e8a1d0f9e158bf4046354cf37c5cd872da
AINULX00159358 <X00159358@myTUDublin.ie>
2023-06-27 07:55:45 +0100

    updated gateway

--- gatewayUrl.sh
+++ gatewayUrl.sh
@@ -4,3 +4,4 @@ export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -
 echo "INGRESS_PORT set to ${INGRESS_PORT}"
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
 echo "GATEWAY_URL set to ${GATEWAY_URL}"
+curl -m 1 "http://${GATEWAY_URL}/"
