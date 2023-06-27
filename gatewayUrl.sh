4138eec02bfd7fe305a43c2a89083cdf68409097
AINULX00159358 <X00159358@myTUDublin.ie>
2023-06-27 08:41:35 +0100

    hard coded

--- gatewayUrl.sh
+++ gatewayUrl.sh
@@ -11,7 +11,9 @@ kubectl get svc
 echo "--------------"
 kubectl get destinationrule
 echo "--------------"
+sleep 10
 istioctl analyze
 echo "----------------------"
+sleep 3
 curl -m 1 "http://${GATEWAY_URL}/"
 echo "    "
