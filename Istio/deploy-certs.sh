78cd9c98dd8062b07b7d186ccfce4988f6a502e1
AINULX00159358 <X00159358@myTUDublin.ie>
2023-07-03 19:08:07 +0100

    added deploy-certs

---
+++ Istio/deploy-certs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+kubectl create namespace istio-system
+kubectl create secret generic cacerts -n istio-system \
+      --from-file=certs/ca-cert.pem \
+      --from-file=certs/ca-key.pem \
+      --from-file=certs/root-cert.pem \
+      --from-file=certs/cert-chain.pem
+kubectl get secret cacerts -n istio-system -o=yaml
+istioctl install -y
+istoctl verify-install
